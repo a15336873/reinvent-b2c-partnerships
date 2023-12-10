a04b75ea2b966552ffabfd142dc05bc66b60e85f
a15336873 <143226062+a15336873@users.noreply.github.com>
2023-12-10 12:42:31 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+        console.log("Random Number: " + item);
+    });
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+    return data;
+
+function main() {
+    const data = generateRandomData();
+main();
+
+    data.forEach(item => {
+    }
+}
+    const data = [];
